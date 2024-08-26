fb559aa7b28f7672fe9a025bfc200545a18fcb3c
Fabrice <schaubfabrice@gmail.com>
2024-08-26 23:10:45 +0200

    added resize callback

--- src/engine/include/window.h
+++ src/engine/include/window.h
@@ -3,13 +3,20 @@
 #include "utils/include/result.h"
 
 #include <GLFW/glfw3.h>
-typedef struct GardenWindow GardenWindow;
-struct GardenWindow {
+
+typedef struct GardenWindowData GardenWindowData;
+struct GardenWindowData {
   int width;
   int height;
+};
+
+typedef struct GardenWindow GardenWindow;
+struct GardenWindow {
+  GardenWindowData data;
   const char *title;
   GLFWwindow *window;
 };
 
 Result garden_window_create(GLFWmonitor *monitor);
+void garden_window_callbacks(GardenWindow *window);
 void garden_window_destroy(GardenWindow *window);
--- src/engine/window.c
+++ src/engine/window.c
@@ -1,6 +1,16 @@
 #include "include/window.h"
 #include <GLFW/glfw3.h>
+#include <stdio.h>
 #include <stdlib.h>
+
+static inline void callback_fb_resize(GLFWwindow *window, int width,
+                                      int height) {
+  GardenWindowData *data = glfwGetWindowUserPointer(window);
+  data->width = width;
+  data->height = height;
+
+  glViewport(0, 0, width, height);
+}
 
 Result garden_window_create(GLFWmonitor *monitor) {
   GardenWindow *window = malloc(sizeof(GardenWindow));
@@ -14,20 +24,33 @@ Result garden_window_create(GLFWmonitor *monitor) {
     return result_error(error_create("Failed to get video mode"));
   }
 
-  window->width = mode->width;
-  window->height = mode->height;
+  window->data.width = mode->width / 2;
+  window->data.height = mode->height / 2;
   window->title = "Garden Simulator";
 
-  window->window = glfwCreateWindow(window->width, window->height,
-                                    window->title, monitor, NULL);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+  window->window = glfwCreateWindow(window->data.width, window->data.height,
+                                    window->title, NULL, NULL);
 
   if (window->window == NULL) {
     garden_window_destroy(window);
     return result_error(error_create("Failed to create window"));
   }
 
+  garden_window_callbacks(window);
+
   return result_ok(window);
 }
+
+void garden_window_callbacks(GardenWindow *window) {
+  glfwSetWindowUserPointer(window->window, &window->data);
+  glfwSetFramebufferSizeCallback(window->window, callback_fb_resize);
+};
 
 void garden_window_destroy(GardenWindow *window) {
   if (window->window != NULL) {
